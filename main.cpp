0b34169024282b8b0a0eaff83da1b75feb586775
1lolka121 <52675570+1lolka121@users.noreply.github.com>
2025-02-27 14:19:02 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	
+	string rasa = "жопа";
+	string name = "Солнышко";
+
+	cout << "Родился новый " << rasa << ", его зовут " << name << ". Добро пожаловать в этот суровый мир!" << endl;
+
+
+}
