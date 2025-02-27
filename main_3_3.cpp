a2af34f701f67a32c176cd6e021d8cf274e55f65
1lolka121 <52675570+1lolka121@users.noreply.github.com>
2025-02-27 23:08:17 +0300

    Update and rename main.cpp to main_3_3.cpp

---
+++ main_3_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	std::cout << "========== выполение 3.3 ==========" << std::endl;
+	std::cout << "========== задание 1 ==========\n" << std::endl;
+	std::string rasa = "Орк", name = "Солнышко";
+	std::cout << "Родился новый " << rasa << ", его зовут " << name << ". Добро пожаловать в этот суровый мир!" << std::endl;
+	std::cout << "\n========== задание 2 ==========\n" << std::endl;
+
+	std::string user_login, user_password;
+	std::cout << "Введите логин : " << std::endl;
+	std::cin >> user_login;
+	std::cout << "Введите пароль : " << std::endl;
+	std::cin >> user_password;
+	std::cout << "----------" << std::endl;
+	std::cout << user_login << ", вы успешно зашли!" << std::endl;
+
+}
