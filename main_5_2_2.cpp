0a5ae20ea981e71f851eaba36c4181c9115a0b7a
1lolka121 <52675570+1lolka121@users.noreply.github.com>
2025-03-01 20:20:21 +0300

    Update main_5_2_2.cpp

--- main_5_2_2.cpp
+++ main_5_2_2.cpp
@@ -6,20 +6,17 @@ int main()
 {
     int amount;
     cout << "input number:"; cin >> amount;
-    if(amount <= 0)
-        return 0;
-    if(amount % 100 == 0) 
+
+
+    if (amount % 100 == 0 && amount <= 100000)
     {
-        if(amount <= 100000)
-            std::cout << "Ok" << std::endl;
-        else
-            std::cout << "Over the limit" << std::endl;
+        std::cout << "Ok" << std::endl;
     }
-    else
-    {
-        std::cout << "Failed: must be multiple of 100" << std::endl;
+    else {
+        std::cout << "Over the limit" << std::endl;
     }
+
 
     return 0;
 }
- 
+
