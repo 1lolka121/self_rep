cf0949691cb2e3808553742f21338c9af9d88e98
1lolka121 <memka133@mail.ru>
2025-03-01 20:10:39 +0300

    изменил код

--- main_5_2_1.cpp
+++ main_5_2_1.cpp
@@ -5,41 +5,30 @@ using namespace std;
 int main()
 {
     int x,y;
-    cout << "input x y:"; cin >> x >> y;
-    if(x > 0)
+    std::cout << "input x y:"; 
+    std::cin >> x >> y;
+    if (x > 0 && y > 0)
     {
-        if(y > 0)
-        {
-            std::cout << "1" << std::endl;
-        }
-        else if(y < 0)
-        {
-            std::cout << "4" << std::endl;
-        }
-        else
-        {
-            std::cout <<"On the border" << std::endl;
-        }
+        std::cout << "1" << std::endl;
     }
-    else if(x < 0)
+    else if(x>0 && y < 0)
+        {
+            std::cout << "4" << std::endl;     
+        }
+
+    else if (x < 0 && y > 0)
     {
-        if(y > 0)
-        {
-            std::cout << "2" << std::endl;
-        }
-        else if(y < 0)
-        {
+        std::cout << "2" << std::endl;
+    }
+    else if(x<0 && y < 0)
+     {
             std::cout << "3" << std::endl;
-        }
+     }
         else
         {
             std::cout << "On the border" << std::endl;
         }
-    }
-    else
-    {
-        std::cout << "On the border" << std::endl;
-    }
+  
     return 0;
 }
  
