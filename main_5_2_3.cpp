1666baaead93da4d8c675c5ef251f16a8fca045e
1lolka121 <52675570+1lolka121@users.noreply.github.com>
2025-03-01 20:24:22 +0300

    Update main_5_2_3.cpp

--- main_5_2_3.cpp
+++ main_5_2_3.cpp
@@ -8,16 +8,14 @@ int main()
     cout << "input year:"; cin >> year;
     if(year <= 0)
         return 0;
-    if(year % 100 == 0)
-    {
-        if(year % 400 == 0)
-            std::cout << "Yes" << std::endl;
-        else
-            std::cout << "No" << std::endl;
-    }
-    else if(year % 4 == 0)
+    if(year % 100 == 0 && year % 400 == 0 && year % 4 == 0)
     {
         std::cout << "Yes" << std::endl;
+       
+    }
+    else 
+    {
+        std::cout << "No" << std::endl;
     }
 
     return 0;
