16a7d6e08b89f7fa0c56028f77906570cd0ab354
1lolka121 <52675570+1lolka121@users.noreply.github.com>
2025-03-01 10:50:10 +0300

    Update main_4_4_1.cpp

--- main_4_4_1.cpp
+++ main_4_4_1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num1, num2;
+    cout << "Введите первое число: ";
+    cin >> num1;
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    cout << "-----Проверяем-----" << endl;
+    if (num1 < num2) {
+        cout << "Наименьшее число: " << num1 << endl;
+    } else if (num2 < num1) {
+        cout << "Наименьшее число: " << num2 << endl;
+    } else {
+        cout << "Числа равны!" << endl;
+    }
+
+    return 0;
+}
