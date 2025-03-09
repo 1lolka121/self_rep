65cfaa2976805f4994c43657b9b8c88d772c8e0b
1lolka121 <52675570+1lolka121@users.noreply.github.com>
2025-03-09 15:44:26 +0300

    Update main5_6_5.cpp

--- main5_6_5.cpp
+++ main5_6_5.cpp
@@ -1,1 +1,23 @@
 
+Задание 5. Усложнение задачи про кирпич
+
+cpp
+Copy
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b, c, m, n, k;
+    cin >> a >> b >> c >> m >> n >> k;
+
+    int box1[] = {a, b, c};
+    int box2[] = {m, n, k};
+    sort(box1, box1 + 3);
+    sort(box2, box2 + 3);
+
+    bool fits = (box1[0] <= box2[0] && box1[1] <= box2[1] && box1[2] <= box2[2]);
+    cout << (fits ? "Можно" : "Нельзя") << endl;
+
+    return 0;
+}
